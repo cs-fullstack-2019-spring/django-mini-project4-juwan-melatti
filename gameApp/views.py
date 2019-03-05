10753daa36e5d1b08f0921a88f35f7839aaefad0
Litterial <yungice12@gmail.com>
2019-03-05 13:13:39 -0600

    comments on views

--- gameApp/views.py
+++ gameApp/views.py
@@ -5,34 +5,34 @@ from django.contrib.auth.models import User
 # Create your views here.
 
 
-def index(request):
-    return render(request,'gameApp/index.html')
+def index(request): #default function
+    return render(request,'gameApp/index.html')   #takes user to the index page
 
-def createAccount(request):
+def createAccount(request):  #allows user to create an account (get request)
 
-    newGameCollectorForm=GameCollectorForm()
+    newGameCollectorForm=GameCollectorForm() #creates a blank form for the account
     context={
 
-        'GCForm':newGameCollectorForm,
+        'GCForm':newGameCollectorForm,   #carries the form into a dictionary
     }
-    return render(request,'gameApp/createAccount.html',context)
+    return render(request,'gameApp/createAccount.html',context) #renders on the create account page
 
-def confirmAccount(request):
+def confirmAccount(request):   #confirms if user-login is valid
     newGameCollectorForm=GameCollectorForm(request.POST)
     context={
 
-        'GCForm':newGameCollectorForm,
+        'GCForm':newGameCollectorForm, #dictionary
     }
 
-    if request.POST['Password1'] == request.POST['Password2']:
-        User.objects.create(request.POST['username'],request.POST['Password1'])
-        return render(request,'gameApp/confirm.html')
+    if request.POST['Password1'] == request.POST['Password2']:  #if the passwords match,
+        User.objects.create(request.POST['username'],request.POST['Password1'])   #create the new user with username and password
+        return render(request,'gameApp/confirm.html')  #renders on a conformation page
 
     else:
-        newGameCollectorForm=GameCollectorForm(request.POST)
-        message="These passwords do not match."
+        newGameCollectorForm=GameCollectorForm(request.POST)   #holds all the information the user entered
+        message="These passwords do not match."  #gives a message that the user
         context={
             'GCForm':newGameCollectorForm,
             'message':message
         }
-        return render(request,'gameApp/createAccount.html',context)
+        return render(request,'gameApp/createAccount.html',context)  #sends all entered information back to the create account page with an error message
